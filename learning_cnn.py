added f1 scores, fixed normalization in lstm

learning_cnn.py
@@ -11,6 +11,9 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Model, Sequential
 from keras.optimizers import Adam
 
+
+from eda import calc_metrics
+
 BATCH_SIZE = 8
 
 
@@ -25,14 +28,15 @@ model = Sequential()
 model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(224,224,3)))
 model.add(Conv2D(32, kernel_size=3, activation='relu'))
 model.add(Flatten())
-model.add(Dense(1, activation='softmax'))
+model.add(Dense(1, activation='sigmoid'))
 model.compile(optimizer='Adam',loss='binary_crossentropy', metrics=['accuracy'])
+# model.add(Dense(6, activation='softmax'))
+# model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 
 
 
-
-train_datagen = ImageDataGenerator(preprocessing_function=preprocess_input, validation_split=0.2)  # included in our dependencies
+train_datagen = ImageDataGenerator(validation_split=0.25)  # included in our dependencies
 
 train_generator = train_datagen.flow_from_directory('data/images/3dgadf/train',
                                                     target_size=(224,224),
@@ -40,14 +44,14 @@ train_generator = train_datagen.flow_from_directory('data/images/3dgadf/train',
                                                     batch_size=BATCH_SIZE,
                                                     class_mode='binary',
                                                     shuffle=True,
-                                                    subset='training') #)
+                                                    subset='training')
 
 validation_generator = train_datagen.flow_from_directory('data/images/3dgadf/test',
                                                          target_size=(224,224),
                                                          color_mode='rgb',
-                                                         batch_size=BATCH_SIZE,
+                                                         batch_size=1,
                                                          class_mode='binary',
-                                                         shuffle=True,
+                                                         shuffle=False,
                                                          subset='validation') # set as validation data
 
 
@@ -60,5 +64,8 @@ step_size_train = train_generator.n//train_generator.batch_size
 step_size_val= validation_generator.n//validation_generator.batch_size
 model.fit_generator(generator=train_generator,
                     steps_per_epoch= step_size_train,
-                    epochs=10,validation_data = validation_generator, validation_steps=step_size_val)
+                    epochs=1, validation_data = validation_generator, validation_steps=step_size_val)
 model.save('data/models/gadf_cnn')
+pred = model.predict_generator(validation_generator, steps=validation_generator.n)
+pred = np.where(pred > 0.5, 1.0, 0.0)
+calc_metrics(validation_generator.classes, pred)

lstm.py
@@ -1,14 +1,13 @@
 from keras.models import Sequential
 from keras.layers import Dropout, LSTM, Dense
-from keras.preprocessing.image import ImageDataGenerator
-
-from sklearn import model_selection
-from sklearn.preprocessing import MinMaxScaler
-import numpy as np
 import pandas as pd
-import pickle
-from sequence_gen import SequenceGen
+import numpy as np
 import os
+import pickle
+
+from sequence_gen import SequenceGen
+from eda import calc_metrics
+
 
 db_file = '/media/wd/data/cells/db.p'
 df = pd.read_pickle(db_file)
@@ -16,6 +15,8 @@ df['layer'] = df['layer'].replace(['6a', '6b'], 6)
 df['layer'] = df['layer'].replace('2/3', 2)
 df['layer'] = df['layer'].astype('int')
 CLASSES = ['spiny', 'aspiny']
+# CLASSES = ['2', '4', '5', '6']
+
 n_classes = len(CLASSES)
 EPOCHS = 10
 
@@ -27,6 +28,8 @@ def get_LSTM_model(n_features, n_steps,  hidden_layer_size=64):
     model.add(Dense(300, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    # model.add(Dense(7, activation='softmax'))
+    # model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
 
@@ -39,6 +42,8 @@ if __name__ == '__main__':
         df = df.drop('490278904_39') # Found to be bad samples
     except:
         pass
+    if n_classes>2:
+        df = df[df['layer'].isin(['2', '4', '5', '6'])]
 
     train_dir = 'data/time_series/train/'
     train_cells = [os.path.splitext(x)[0] for x in os.listdir(train_dir)]
@@ -48,11 +53,21 @@ if __name__ == '__main__':
     test_idx = df.index.isin(test_cells)
 
     train_generator = SequenceGen(train_dir, df.index[train_idx], df['dendrite_type'][train_idx].cat.codes)
-    test_generator = SequenceGen(test_dir, df.index[test_idx], df['dendrite_type'][test_idx].cat.codes)
+    test_generator = SequenceGen(test_dir, df.index[test_idx], df['dendrite_type'][test_idx].cat.codes, batch_size=1,
+                                 shuffle=False)
+    # df['layer'] = df['layer'].apply(lambda x: to_categorical(x, num_classes=7))
+    # train_generator = SequenceGen(train_dir, df.index[train_idx], df['layer'][train_idx])
+    # test_generator = SequenceGen(test_dir, df.index[test_idx], df['layer'][test_idx])
 
     n_feats = 1
     n_steps = 450000
     model1 = get_LSTM_model(n_steps, n_feats)
     model1.fit_generator(generator=train_generator,
-                         validation_data=test_generator, epochs=EPOCHS)
+                         epochs=EPOCHS)
     model1.save('data/models/lstm')
+    scaling_data = (train_generator.scale_mean, train_generator.scale_std)
+    with open('data/models/lstm_scaling.p','wb') as f:
+        pickle.dump(scaling_data, f)
+    pred = model1.predict_generator(test_generator)
+    pred = np.where(pred > 0.5, 1.0, 0.0)
+    calc_metrics(df['dendrite_type'][test_idx].cat.codes.values, pred)

sequence_gen.py
@@ -1,9 +1,10 @@
-from keras.utils import Sequence, to_categorical
-from sklearn.preprocessing import scale
+from keras.utils import Sequence
 import numpy as np
 import os
 
 class SequenceGen(Sequence):
+    scale_mean = None
+    scale_std = None
     'Generates data for Keras'
     def __init__(self, data_dir, ids, labels, batch_size=16, dim=(1, 450000),
                  shuffle=True):
@@ -15,6 +16,11 @@ class SequenceGen(Sequence):
         self.ids = ids
         self.n_classes = len(np.unique(labels))
         self.shuffle = shuffle
+        if self.scale_mean is None:
+            SequenceGen.scale_mean = np.zeros(self.dim)
+            SequenceGen.scale_std = np.zeros(self.dim)
+        self.epoch = 0
+        self.index_complete = None
         self.on_epoch_end()
 
     def __len__(self):
@@ -24,6 +30,7 @@ class SequenceGen(Sequence):
     def __getitem__(self, index):
         'Generate one batch of data'
         # Generate indexes of the batch
+        self.index_complete += 1
         indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
 
         # Find list of IDs
@@ -31,7 +38,6 @@ class SequenceGen(Sequence):
 
         # Generate data
         X, y = self.__data_generation(ids_temp)
-
         return X, y
 
     def on_epoch_end(self):
@@ -39,12 +45,14 @@ class SequenceGen(Sequence):
         self.indexes = np.arange(len(self.ids))
         if self.shuffle:
             np.random.shuffle(self.indexes)
+        self.epoch += 1
+        self.index_complete = 0
 
     def __data_generation(self, ids_temp):
         'Generates data containing batch_size samples'  # X : (n_samples, *dim, n_channels)
         # Initialization
         X = np.empty((self.batch_size, *self.dim))
-        y = np.empty(self.batch_size, dtype=int)
+        y = np.empty((self.batch_size, 1))
 
         # Generate data
         for i, ID in enumerate(ids_temp):
@@ -53,8 +61,30 @@ class SequenceGen(Sequence):
 
             # Store class
             y[i] = self.labels[ID]
-        X = np.expand_dims(scale(X.squeeze()), 1)
+
+        if self.batch_size > 1:
+            scaled_x = self.scale_batch(X)
+        else:
+            scaled_x = self.scale(X)
+        X = np.reshape(scaled_x, X.shape)
         return X, y
+
+    def scale_batch(self, x):
+        mean = np.mean(x, axis=0)
+        std = np.std(x, axis=0)
+        samples_seen = (self.epoch-1) * len(self.indexes) + (self.index_complete-1) * self.batch_size
+        # https://notmatthancock.github.io/2017/03/23/simple-batch-stat-updates.html
+        SequenceGen.scale_std = np.sqrt(samples_seen/(samples_seen+ self.batch_size)* SequenceGen.scale_std**2 +
+                                        self.batch_size/(samples_seen+ self.batch_size)*std**2 +\
+                                        samples_seen * self.batch_size/(samples_seen+ self.batch_size)**2 *
+                                        (SequenceGen.scale_mean - mean)**2)
+        SequenceGen.scale_mean  = ((samples_seen * SequenceGen.scale_mean) + (self.batch_size * mean)) / (samples_seen + self.batch_size)
+        return self.scale(x)
+
+    def scale(self, x):
+        x -= self.scale_mean
+        x /= self.scale_std
+        return x
 
 if __name__ == '__main__':
     import pandas as pd